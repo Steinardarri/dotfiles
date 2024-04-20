ba699254f2f679722ca58ac8df0a7c2733aecb8d
Steinar Darri Þorgilsson <steinardth@gmail.com>
2024-04-20 13:50:35 +0000

    NixOS Budgie flake config, swap in progress

---
+++ configuration.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        consoleMode = "auto";
+      };
+      efi.canTouchEfiVariables = true;
+      timeout = 3;
+    };
+    initrd.luks.devices."luks-408d55bf-2ccf-4a3a-8bf6-e18de61188ce".device = "/dev/disk/by-uuid/408d55bf-2ccf-4a3a-8bf6-e18de61188ce";
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Networking
+  networking = {
+    hostName = "steinar-lappi";
+    networkmanager.enable = true;
+  };
+
+  # Locale
+  time.timeZone = "Atlantic/Reykjavik";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "is_IS.UTF-8";
+    LC_IDENTIFICATION = "is_IS.UTF-8";
+    LC_MEASUREMENT = "is_IS.UTF-8";
+    LC_MONETARY = "is_IS.UTF-8";
+    LC_NAME = "is_IS.UTF-8";
+    LC_NUMERIC = "is_IS.UTF-8";
+    LC_PAPER = "is_IS.UTF-8";
+    LC_TELEPHONE = "is_IS.UTF-8";
+    LC_TIME = "is_IS.UTF-8";
+  };
+
+  console.keyMap = "is-latin1";
+
+  ### Budgie ###
+
+  # Enable the X11 windowing system
+  services.xserver.enable = true;
+
+  # Enable the Budgie Desktop environment
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.budgie.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "is";
+    variant = "";
+    options = "caps:swapescape";
+  };
+
+  # Enable CUPS to print documents
+  services.printing.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Enable sound with pipewire
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  users.users.steinardth = {
+    isNormalUser = true;
+    description = "Steinar Darri";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      opera
+    ];
+  };
+
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "steinardth";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    unzip
+    just
+    curl
+    gparted
+    barrier
+  ];
+
+  system.stateVersion = "23.11";
+
+}
---
+++ flake.nix
@@ -0,0 +1,91 @@
+{
+  description = "Your new nix config";
+  # https://github.com/Misterio77/nix-starter-configs
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-colors.url = "github:misterio77/nix-colors";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    # Your custom packages
+    # Accessible through 'nix build', 'nix shell', etc
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    # Formatter for your nix files, available through 'nix fmt'
+    # Other options beside 'alejandra' include 'nixpkgs-fmt'
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    # Your custom packages and modifications, exported as overlays
+    overlays = import ./overlays {inherit inputs;};
+    # Reusable nixos modules you might want to export
+    # These are usually stuff you would upstream into nixpkgs
+    nixosModules = import ./modules/nixos;
+    # Reusable home-manager modules you might want to export
+    # These are usually stuff you would upstream into home-manager
+    homeManagerModules = import ./modules/home-manager;
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      your-hostname = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./nixos/configuration.nix
+        ];
+      };
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./configuration.nix
+          inputs.home-manager.nixosModules.home-manager {
+            home-manager.extraSpecialArgs = {
+              inherit username; inherit inputs;
+              inherit host;
+              inherit (inputs.nix-colors.lib-contrib {inherit pkgs;}) gtkThemeFromScheme;
+            };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.steinardth = import ./home-manager/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
