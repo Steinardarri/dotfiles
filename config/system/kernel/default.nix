868e1ce8105318f9e7585bfacaaec6a22171bff0
SteinarDarri <steinar@steinardth.xyz>
2024-06-17 22:54:35 +0000

    ❌ REMOVE: Unused kernels

--- config/system/kernel/default.nix
+++ config/system/kernel/default.nix
@@ -1,14 +1,8 @@
 {...}: {
   imports = [
     ./latest.nix
-    ./lqx.nix
     ./vanilla.nix
     ./xanmod.nix # My choice
     ./zen.nix
-
-    # For the adventurous people
-    # It's not listed in the list of possible options,
-    # but the ones who want to try it can do so.
-    ./testing.nix
   ];
 }
