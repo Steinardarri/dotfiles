01b4b7985758f9d09330b6934d0eefe2890896f5
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-07-25 23:51:03 +0000

    ✨  Audio module

---
+++ modules/system/audio.nix
@@ -0,0 +1,26 @@
+{pkgs, ...}: {
+  services = {
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+    blueman.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    bluez
+    bluez-tools
+    blueman
+    easyeffects
+    pipewire
+    wireplumber
+    pavucontrol
+    pamixer
+    playerctl
+  ];
+}
--- modules/system/default.nix
+++ modules/system/default.nix
@@ -9,6 +9,7 @@
     ./programs
     ./services
 
+    ./audio.nix
     ./boot.nix
     ./locale.nix
     ./packages.nix
