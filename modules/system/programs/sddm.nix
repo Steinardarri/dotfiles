254612f18bfe3584f472b5e4b4d308b0e9ebdc56
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-07-26 00:09:01 +0000

    ✨  sddm Module Completed

--- modules/system/programs/sddm.nix
+++ modules/system/programs/sddm.nix
@@ -1,10 +1,35 @@
-{username, ...}: {
-  services.displayManager.sddm = {
-    autoNumlock = true;
-  };
-
+{
+  pkgs,
+  username,
+  ...
+}: {
   services.displayManager = {
     autoLogin.enable = true;
     autoLogin.user = "${username}";
+
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+      autoNumlock = true;
+      extraPackages = with pkgs; [
+        kdePackages.sddm-kcm
+        kdePackages.qtsvg
+        kdePackages.qtmultimedia
+        kdePackages.qtvirtualkeyboard
+        kdePackages.qtquickcontrols2
+        kdePackages.qtgraphicaleffects
+        kdePackages.layer-shell-qt
+        kdePackages.qtwayland
+      ];
+      settings = {
+        General = {
+          # Set default session globally
+          DefaultSession = "hyprland.desktop";
+        };
+        Wayland = {
+          EnableHiDPI = true;
+        };
+      };
+    };
   };
 }
