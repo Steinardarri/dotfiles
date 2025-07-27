4403b49b68a2a28c78f90684f269c4eb3bd858c6
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-07-27 16:02:36 +0000

    🐛  Filesystem collision with disko

--- hosts/heima/hardware-configuration.nix
+++ hosts/heima/hardware-configuration.nix
@@ -13,20 +13,21 @@
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
-  fileSystems."/" = {
+  fileSystems."/" = lib.mkDefault {
     device = "/dev/disk/by-uuid/6c07be92-6fad-4edb-abfb-f4f03a66a778";
     fsType = "xfs";
   };
 
-  boot.initrd.luks.devices."cryptNix".device = "/dev/disk/by-uuid/911a53b6-e547-4848-8f14-e84b6ce02cbb";
+  boot.initrd.luks.devices."cryptNix".device = lib.mkDefault "/dev/disk/by-uuid/911a53b6-e547-4848-8f14-e84b6ce02cbb";
 
-  fileSystems."/boot" = {
+  fileSystems."/boot" = lib.mkDefault {
     device = "/dev/disk/by-uuid/C1C8-6AEA";
     fsType = "vfat";
     options = ["fmask=0077" "dmask=0077"];
   };
 
-  swapDevices = [];
+  swapDevices = lib.mkForce [];
+  zramSwap.enable = true;
 
   networking.useDHCP = lib.mkDefault true;
   networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
