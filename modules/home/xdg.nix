bebfc7ca3bc6a8fdbf7248e89003b8b3262589c8
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-07-27 17:51:36 +0000

    🔥  Xdg package conflicts

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -9,13 +9,11 @@
     portal = {
       enable = true;
       extraPortals = with pkgs; [
-        xdg-desktop-portal-hyprland
         xdg-desktop-portal-gtk
         xdg-desktop-portal
       ];
       xdgOpenUsePortal = true;
       configPackages = with pkgs; [
-        xdg-desktop-portal-hyprland
         xdg-desktop-portal-gtk
         xdg-desktop-portal
       ];
