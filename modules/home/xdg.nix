8a02818323daaff11beddffb452d28c9fd056847
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-07-31 13:52:28 +0000

    ⚡ Make Zen default browser

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -15,11 +15,19 @@
       configPackages = with pkgs; [
         xdg-desktop-portal-gtk
       ];
-      config.common.default = [ "hyprland" ];
+      config.common.default = ["hyprland"];
     };
 
+    mime.enable = true;
     mimeApps = {
       enable = true;
+      defaultApplications = {
+        "text/html" = "zen.desktop";
+        "x-scheme-handler/http" = "zen.desktop";
+        "x-scheme-handler/https" = "zen.desktop";
+        "x-scheme-handler/about" = "zen.desktop";
+        "x-scheme-handler/unknown" = "zen.desktop";
+      };
     };
 
     userDirs = {
