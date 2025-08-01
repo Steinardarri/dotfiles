a655e65b1f9bf8a485ea10fd754a50a9581c741b
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-08-01 00:26:35 +0000

    ⚡ Kitty changes

--- modules/home/gui/kitty.nix
+++ modules/home/gui/kitty.nix
@@ -5,18 +5,21 @@
     shellIntegration.enableFishIntegration = true;
 
     settings = {
-      "bold_font" = "auto";
-      "italic_font" = "auto";
-      "bold_italic_font" = "auto";
-      "enable_audio_bell" = "no";
-      "font_size" = "14.0";
-      "window_padding_width" = "25";
-      "cursor_trail" = "1";
+      enable_audio_bell = "no";
+      font_size = "14.0";
+      window_padding_width = "25";
+      scrollback_lines = 10000;
 
-      "tab_bar_edge" = "bottom";
-      "tab_bar_style" = "powerline";
-      "tab_powerline_style" = "slanted";
-      "tab_title_template" = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+      cursor_shape = "beam";
+      cursor_shape_unfocused = "hollow";
+      cursor_blink_interval = "0.5 ease-in-out";
+      cursor_stop_blinking_after = 15;
+      cursor_trail = "1";
+
+      tab_bar_edge = "bottom";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
     };
   };
 }
