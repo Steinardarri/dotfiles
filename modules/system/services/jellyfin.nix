2ab05791a881c1da3489dd6aee4267c36ea19d2e
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-08-02 22:50:08 +0000

    ⚡ Jellyfin tweaks

--- modules/system/services/jellyfin.nix
+++ modules/system/services/jellyfin.nix
@@ -12,5 +12,21 @@
       enable = true;
       openFirewall = true;
     };
+
+    users.users.jellyfin.extraGroups = [
+      "video"
+      "render"
+    ];
+
+    networking.firewall = {
+      allowedTCPPorts = [
+        8096 # http traffic
+      ];
+
+      allowedUDPPorts = [
+        1900 # service discovery
+        7359 # client discovery
+      ];
+    };
   };
 }
