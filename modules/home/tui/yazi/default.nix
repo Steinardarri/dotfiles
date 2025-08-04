f05deb5301261b559b25c740b2d34642821d6765
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-08-04 00:50:24 +0000

    ⚡ Yazi improvements to try

--- modules/home/tui/yazi/default.nix
+++ modules/home/tui/yazi/default.nix
@@ -1,52 +1,58 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.yazi = {
     enable = true;
     initLua = ./yaziInit.lua;
+    enableFishIntegration = true;
+    shellWrapperName = "yy";
     plugins = {
       full-border = pkgs.yaziPlugins.full-border;
-      git = pkgs.yaziPlugins.git;
       starship = pkgs.yaziPlugins.starship;
     };
     settings = {
-      mgr = {
+      manager = {
         ratio = [1 3 3];
         sort_by = "natural";
+        sort_sensitive = true;
+        sort_reverse = false;
+        sort_dir_first = true;
         show_hidden = true;
+        show_symlink = true;
         linemode = "size_and_mtime";
       };
-      opener = {
-        edit = [
-          {
-            run = "$EDITOR '$@'";
-            desc = "$EDITOR";
-            block = true;
-            for = "unix";
-          }
-          {
-            run = "sudo $EDITOR '$@'";
-            desc = "sudo $EDITOR";
-            block = true;
-            for = "unix";
-          }
-        ];
+
+      preview = {
+        tab_size = 3;
+        image_filter = "lanczos3";
+        max_width = 600;
+        max_height = 900;
+        image_quality = 90;
+        cache_dir = config.xdg.cacheHome;
       };
+
+      # opener = {
+      #   edit = [
+      #     {
+      #       run = "$EDITOR '$@'";
+      #       desc = "$EDITOR";
+      #       block = true;
+      #       for = "unix";
+      #     }
+      #     {
+      #       run = "sudo $EDITOR '$@'";
+      #       desc = "sudo $EDITOR";
+      #       block = true;
+      #       for = "unix";
+      #     }
+      #   ];
+      # };
 
       input = {
         cursor_blink = true;
       };
-
-      plugin.prepend_fetchers = [
-        {
-          id = "git";
-          name = "*";
-          run = "git";
-        }
-        {
-          id = "git";
-          name = "*/";
-          run = "git";
-        }
-      ];
     };
 
     keymap = {
