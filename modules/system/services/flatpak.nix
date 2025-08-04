45191bda5527ac695c4144af20cac3e9d2899cec
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-08-04 09:50:42 +0000

    🐛 Flatpak service firing too soon

--- modules/system/services/flatpak.nix
+++ modules/system/services/flatpak.nix
@@ -12,7 +12,7 @@
     services.flatpak.enable = true;
 
     systemd.services.flatpak-repo = {
-      wantedBy = ["multi-user.target"];
+      wantedBy = ["multi-user.target" "network-online.target"];
       path = [pkgs.flatpak];
       script = ''
         flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
