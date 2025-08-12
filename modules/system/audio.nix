d648643ca4bf8327fae63b27c1f9ef8254f25082
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-08-12 11:05:51 +0000

    🔧 Not forcing puls audio false

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -17,7 +17,7 @@
       # https://www.reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/
       wireplumber.extraConfig."wireplumber.profiles".main."monitor.libcamera" = "disabled";
     };
-    pulseaudio.enable = lib.mkForce false;
+    pulseaudio.enable = false;
   };
 
   environment.systemPackages = with pkgs; [
