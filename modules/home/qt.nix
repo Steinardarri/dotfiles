17ab323937b2eefe4d4810d28a598b4f54ac8267
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-08-16 11:35:26 +0000

    🐛 Wrong QT QPA var

--- modules/home/qt.nix
+++ modules/home/qt.nix
@@ -17,7 +17,7 @@
 
   home.sessionVariables = {
     QT_AUTO_SCREEN_SCALE_FACTOR = "1"; # (From the Qt documentation) enables automatic scaling, based on the monitor’s pixel density
-    QT_QPA_PLATFORM = "wayland,x11"; # Tell Qt applications to use the Wayland backend
+    QT_QPA_PLATFORM = "wayland"; # Tell Qt applications to use the Wayland backend
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1"; # Disables window decorations on Qt applications
   };
 }
