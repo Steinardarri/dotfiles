886eafa07fb39b70e59a8814fde256d38bc88d00
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-09-07 16:56:20 +0000

    ⚡ More fix for network manager

--- hosts/heima/network.nix
+++ hosts/heima/network.nix
@@ -3,18 +3,20 @@
 
   networking = {
     # Disable NetworkManager's internal DNS resolution
-    networkmanager.dns = lib.mkForce "none";
+    networkmanager = {
+      insertNameservers = [
+        "192.168.31.43"
+      ];
+      # dns = lib.mkForce "none";
+    };
 
     # These options are unnecessary when managing DNS ourselves
-    dhcpcd.enable = false;
-    useDHCP = false;
-
-    # Configure DNS servers manually
-    nameservers = [
-      "192.168.31.43"
-    ];
+    # interfaces.enp6s0.useDHCP = false;
+    # dhcpcd.enable = false;
+    # useDHCP = false;
 
     enableIPv6 = false;
+    # nftables.enable = lib.mkForce false;
   };
-  services.resolved.enable = lib.mkForce false;
+  # services.resolved.enable = lib.mkForce false;
 }
